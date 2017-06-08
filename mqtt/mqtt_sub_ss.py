2e70dab3f0aa70363cf9e00f05843cff3d795de1
smkim912 <zinjja2kw@gmail.com>
2017-06-08 15:37:07 +0900

    MQTT shinshinsa

---
+++ mqtt/mqtt_sub_ss.py
@@ -0,0 +1,18 @@
+import paho.mqtt.client as mqtt
+
+
+def on_connect(client, userdata, rc):
+  print ("Connected with result coe " + str(rc))
+  client.subscribe("GW01")
+
+
+def on_message(client, userdata, msg):
+  print "Topic: ", msg.topic + '\nMessage: ' + str(msg.payload)
+
+client = mqtt.Client()       
+client.on_connect = on_connect 
+client.on_message = on_message  
+
+client.connect("1.214.215.226", 1883, 60)  
+
+client.loop_forever()
