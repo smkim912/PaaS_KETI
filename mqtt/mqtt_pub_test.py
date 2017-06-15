b139d711c4c5aac07bfe94de8fa0d35e7287f9d0
smkim912 <zinjja2kw@gmail.com>
2017-06-15 22:37:04 +0900

    Arrange files

--- mqtt/mqtt_pub_test.py
+++
@@ -1,7 +0,0 @@
-import paho.mqtt.client as mqtt
-
-mqttc = mqtt.Client("python_pub")  
-#mqttc.connect("test.mosquitto.org", 1883) 
-mqttc.connect("1.214.215.226", 1883)
-mqttc.publish("GW01", "Hello World!")
-mqttc.loop(2)      
